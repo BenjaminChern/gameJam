5a197a2ed23a602e781a7c4afddf00894d1171d4
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2022-03-28 11:45:53 -0400

    fixed heart issue

--- Score_Space/Assets/Scripts/HeartScript.cs
+++ Score_Space/Assets/Scripts/HeartScript.cs
@@ -5,21 +5,75 @@ using UnityEngine.UI;
 
 public class HeartScript : MonoBehaviour
 {
-    public GameObject canvas;
+    private GameObject canvas;
     public Texture2D heart;
     public Texture2D halfHeart;
     public Texture2D noHeart;
+
+    private GameObject heartObject1;
+    private GameObject heartObject2;
+    private GameObject heartObject3;
+
+    private RectTransform trans1;
+    private RectTransform trans2;
+    private RectTransform trans3;
+
+    private Image image1;
+    private Image image2;
+    private Image image3;
 
     private int leftMost = -375;
     private int height = 150;
     private int spacing = 30;
 
-    public GameObject[] hearts;
+    private GameObject[] hearts;
+    private int health = 6;
     //public Sprite refSprite;
 
     // Use this for initialization
     void Start()
     {
+
+        canvas = gameObject;
+
+        heartObject1 = new GameObject("heart1");
+        heartObject2 = new GameObject("heart2");
+        heartObject3 = new GameObject("heart3");
+
+        trans1 = heartObject1.AddComponent<RectTransform>();
+        trans1.transform.SetParent(canvas.transform); // setting parent
+        trans1.localScale = Vector3.one;
+        trans1.anchoredPosition = new Vector2(-375, 150); // setting position, will be on center
+        trans1.sizeDelta = new Vector2(30, 30); // custom size
+
+        trans2 = heartObject2.AddComponent<RectTransform>();
+        trans2.transform.SetParent(canvas.transform); // setting parent
+        trans2.localScale = Vector3.one;
+        trans2.anchoredPosition = new Vector2(-345, 150); // setting position, will be on center
+        trans2.sizeDelta = new Vector2(30, 30); // custom size
+
+        trans3 = heartObject3.AddComponent<RectTransform>();
+        trans3.transform.SetParent(canvas.transform); // setting parent
+        trans3.localScale = Vector3.one;
+        trans3.anchoredPosition = new Vector2(-315, 150); // setting position, will be on center
+        trans3.sizeDelta = new Vector2(30, 30); // custom size
+
+
+        image1 = heartObject1.AddComponent<Image>();
+        image1.sprite = Sprite.Create(heart, new Rect(0, 0, heart.width, heart.height), new Vector2(0.5f, 0.5f));
+        
+
+        image2 = heartObject2.AddComponent<Image>();
+        image2.sprite = Sprite.Create(heart, new Rect(0, 0, heart.width, heart.height), new Vector2(0.5f, 0.5f));
+        
+
+        image3 = heartObject3.AddComponent<Image>();
+        image3.sprite = Sprite.Create(heart, new Rect(0, 0, heart.width, heart.height), new Vector2(0.5f, 0.5f));
+        //imgObject.transform.SetParent(canvas.transform);
+
+
+
+        /*
         int count = 0;
         hearts = new GameObject[3];
         for(int i = 0; i < 3; i++)
@@ -30,6 +84,7 @@ public class HeartScript : MonoBehaviour
         {
             print(hearts[i]);
         }
+        */
 
 
 
@@ -37,13 +92,21 @@ public class HeartScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        print(canvas.gameObject.transform.GetChild(0).gameObject);
+        print(health);
+
+        if(health >= 6)
+        {
+            image1.sprite = Sprite.Create(heart, new Rect(0, 0, heart.width, heart.height), new Vector2(0.5f, 0.5f));
+        }
+
+        image1.sprite = Sprite.Create(halfHeart, new Rect(0, 0, heart.width, heart.height), new Vector2(0.5f, 0.5f));
+
     }
 
-    public void healthSet(int health)
+    /*
+    void updateHearts(int health)
     {
-        print("test for child in method:  " + canvas.gameObject.transform.GetChild(0).gameObject);
-        print("health is:       " + health);
         for (int i = 0; i < hearts.Length; i++)
         {
             print("inside method:    " + hearts[i]);
@@ -51,12 +114,12 @@ public class HeartScript : MonoBehaviour
         for (int i = 0; i < 3; i++)
         {
             print(hearts);
-            /*if (hearts.Length > 0)
+            if (hearts.Length > 0)
             {
                 Destroy(hearts[i]);
             }
-            */
-            Destroy(canvas.gameObject.transform.GetChild(i).gameObject);
+            
+            //Destroy(canvas.gameObject.transform.GetChild(i).gameObject);
 
 
             if (health >= i * 2 + 2)
@@ -71,9 +134,20 @@ public class HeartScript : MonoBehaviour
             }
         }
     }
+    */
 
+    public void healthSet(int health)
+    {
+        //print("test for child in method:  " + canvas.gameObject.transform.GetChild(0).gameObject);
+        //print("health is:       " + health);
+
+        //updateHearts(health);
+        this.health = health;
+    }
+    /*
     void makeHeart(int x, int y, Texture2D tex, string name, int position, GameObject[] hearts)
     {
+        canvas = gameObject;
         GameObject imgObject = new GameObject(name);
 
         RectTransform trans = imgObject.AddComponent<RectTransform>();
@@ -89,4 +163,5 @@ public class HeartScript : MonoBehaviour
         print("here" + hearts[position] + "   " + hearts.Length);
         print("test for child:  " + canvas.gameObject.transform.GetChild(position).gameObject);
     }
+    */
 }
