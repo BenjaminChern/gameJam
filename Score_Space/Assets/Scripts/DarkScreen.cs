c14b6f0f2a2e0df1ae7dc5e0c354db3e5e63f270
Cole Kaplan <44578832+ColeKaplan@users.noreply.github.com>
2022-04-05 14:56:04 -0400

    adding a dark screen on death

---
+++ Score_Space/Assets/Scripts/DarkScreen.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DarkScreen : MonoBehaviour
+{
+    private GameObject canvas;
+    public Texture2D black;
+    private GameObject blackSquare;
+    private RectTransform trans;
+    private Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = gameObject;
+
+        blackSquare = new GameObject("black");
+        trans = blackSquare.AddComponent<RectTransform>();
+        trans.transform.SetParent(canvas.transform); // setting parent
+        trans.localScale = Vector3.one;
+        trans.anchoredPosition = new Vector2(0, 0); // setting position, will be on center
+        trans.sizeDelta = new Vector2(2000, 2000); // custom size
+
+        image = blackSquare.AddComponent<Image>();
+        image.sprite = Sprite.Create(black, new Rect(0, 0, black.width, black.height), new Vector2(0.5f, 0.5f));
+
+       // blackSquare.GetComponent<MeshRenderer>().material.color.Seta = .1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
